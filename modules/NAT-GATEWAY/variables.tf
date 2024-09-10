db40c1e5e81b35be793ade39c89c26ba786bf845
Otillia Kawuma <kawumatk@gmail.com>
2024-09-10 15:48:32 +0100

    modules removed

--- modules/NAT-GATEWAY/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "public_subnet_az1_id" {}
-variable "aws_internet_gateway" {}
-variable "vpc_id" {}
-variable "public_subnet_az2_id" {}
-variable "private_app_subnet_az1_id" {}
-variable "private_db_subnet_az1_id" {}
-variable "private_app_subnet_az2_id" {}
-variable "private_db_subnet_az2_id" {}
--- modules/security-group/output.tf
+++
@@ -1,7 +0,0 @@
-output "alb_security_group_id" {
-    value = aws_security_group.alb-security-group.id 
-}
-
-output "ecs_security_group_id" {
-    value = aws_security_group.ecs_security_group.id 
-}
