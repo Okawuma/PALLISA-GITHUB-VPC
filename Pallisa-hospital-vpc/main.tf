91606e6c1d1fce135feab8a3c024cbe8ae0fb710
Otillia Kawuma <kawumatk@gmail.com>
2024-09-12 12:39:58 +0100

    created Pallisa-Nat-gateway

--- Pallisa-hospital-vpc/main.tf
+++ Pallisa-hospital-vpc/main.tf
@@ -18,3 +18,15 @@ module "pallisa-root-vpc" {
   private_db_subnet_az1_cidr  = var.private_db_subnet_az1_cidr
   private_db_subnet_az2_cidr  = var.private_db_subnet_az2_cidr
 }
+# created Nat-gateway
+module "pallisa-nat-gateway" {
+  source                    = "../modules/Nat-gateway"
+  public_subnet_az1_id      = module.pallisa-root-vpc.public_subnet_az1_id
+  aws_internet_gateway      = module.pallisa-root-vpc.aws_internet_gateway
+  vpc_id                    = module.pallisa-root-vpc.vpc_id
+  public_subnet_az2_id      = module.pallisa-root-vpc.public_subnet_az2_id
+  private_app_subnet_az1_id = module.pallisa-root-vpc.private_app_subnet_az1_id
+  private_db_subnet_az1_id  = module.pallisa-root-vpc.private_db_subnet_az1_id
+  private_app_subnet_az2_id = module.pallisa-root-vpc.private_app_subnet_az2_id
+  private_db_subnet_az2_id  = module.pallisa-root-vpc.private_db_subnet_az2_id
+}
