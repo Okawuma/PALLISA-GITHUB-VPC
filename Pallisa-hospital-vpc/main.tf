c6522c268d3528a9ee5c38da146933baa8999a84
Otillia Kawuma <kawumatk@gmail.com>
2024-09-15 14:53:31 +0100

    arg-change

--- Pallisa-hospital-vpc/main.tf
+++ Pallisa-hospital-vpc/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 # create vpc for pallisa hospital using the vpc root module.
 # reference the root vpc module
-module "pallisa-root-vpc" {
+module "pallisa-hospital-vpc" {
   source                      = "../modules/pallisa-root-vpc"
   region                      = var.region
   project_name                = var.project_name
@@ -18,15 +18,15 @@ module "pallisa-root-vpc" {
   private_db_subnet_az1_cidr  = var.private_db_subnet_az1_cidr
   private_db_subnet_az2_cidr  = var.private_db_subnet_az2_cidr
 }
-# created Nat-gateway
-module "pallisa-nat-gateway" {
+# created Pallisa-Nat-gateway
+module "Nat-gateway" {
   source                    = "../modules/Nat-gateway"
-  public_subnet_az1_id      = modules.pallisa-hospital-vpc.public_subnet_az1_id
-  aws_internet_gateway      = modules.pallisa-hospital-vpc.aws_internet_gateway
-  vpc_id                    = modules.pallisa-hospital-vpc.vpc_id
-  public_subnet_az2_id      = modules.pallisa-hospital-vpc.public_subnet_az2_id
-  private_app_subnet_az1_id = modules.pallisa-hospital-vpc.private_app_subnet_az1_id
-  private_db_subnet_az1_id  = modules.pallisa-hospital-vpc.private_db_subnet_az1_id
-  private_app_subnet_az2_id = modules.pallisa-hospital-vpc.private_app_subnet_az2_id
-  private_db_subnet_az2_id  = modules.pallisa-hospital-vpc.private_db_subnet_az2_id
+  public_subnet_az1_id      = module.pallisa-hospital-vpc.public_subnet_az1_id
+  aws_internet_gateway      = module.pallisa-hospital-vpc.aws_internet_gateway
+  vpc_id                    = module.pallisa-hospital-vpc.vpc_id
+  public_subnet_az2_id      = module.pallisa-hospital-vpc.public_subnet_az2_id
+  private_app_subnet_az1_id = module.pallisa-hospital-vpc.private_app_subnet_az1_id
+  private_db_subnet_az1_id  = module.pallisa-hospital-vpc.private_db_subnet_az1_id
+  private_app_subnet_az2_id = module.pallisa-hospital-vpc.private_app_subnet_az2_id
+  private_db_subnet_az2_id  = module.pallisa-hospital-vpc.private_db_subnet_az2_id
 }
