6e742fe592e0d8a7ad98b8f09418272d40845dce
Otillia Kawuma <kawumatk@gmail.com>
2024-09-15 22:51:19 +0100

    created sec-grp module

--- Pallisa-hospital-vpc/main.tf
+++ Pallisa-hospital-vpc/main.tf
@@ -30,3 +30,19 @@ module "Nat-gateway" {
   private_app_subnet_az2_id = module.pallisa-hospital-vpc.private_app_subnet_az2_id
   private_db_subnet_az2_id  = module.pallisa-hospital-vpc.private_db_subnet_az2_id
 }
+
+# created security group
+module "sec_group" {
+  source = "../modules/sec-group"
+  vpc_id = module.pallisa-hospital-vpc.vpc_id
+}
+
+# created application load balancer
+module "app-load-balancer" {
+  source                = "../modules/app-load-balancer"
+  project_name          = module.pallisa-hospital-vpc.project_name
+  alb_security_group_id = module.sec_group.alb_security_group_id
+  public_subnet_az1_id  = module.pallisa-hospital-vpc.private_db_subnet_az1_id
+  public_subnet_az2_id  = module.pallisa-hospital-vpc.public_subnet_az2_id
+  vpc_id                = module.pallisa-hospital-vpc.vpc_id
+}
